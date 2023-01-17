986504689f125b4d7651f3620fa10466372d253c
MoizAhmed2517 <111642632+MoizAhmed2517@users.noreply.github.com>
2023-01-17 23:47:49 +0500

    1/17/2023-V13

--- src/components/groups/Login.js
+++ src/components/groups/Login.js
@@ -1,31 +1,167 @@
-import { Box, Grid } from '@mui/material'
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import banner from '../../static/images/Logo/logo.jpg';
+
+function createData(email, password) {
+  return {email, password};
+}
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 const Login = () => {
+
+  const [credentials, setCredentials] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [user, setUser] = useState('student');
+
+  const handleChange = (event) => {
+    setUser(event.target.value);
+  };
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    setCredentials({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  console.log(user)
+
   return (
-    <>
-    <Box sx={{
-        bgcolor: 'blue',
-        height: '100vh',
-        width: '50%',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-    }}>
-        Image
-    </Box>
-    <Box sx={{
-        bgcolor: 'green',
-        height: '100vh',
-        width: '50%',
-        position: 'absolute',
-        top: '0',
-        right: '0',
-    }}>
-        Login Functionality WIll be here
-    </Box>
-    </> 
-  )
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} sx={{
+            backgroundImage: `url(${banner})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: '#F39223' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" sx={{ color: '#F39223', fontWeight: 'bold' }}>
+              Hi! Welcome Back
+            </Typography>
+            <FormControl sx={{ m: 1, width: "100%", display: 'flex', justifyContent: 'center' }}>
+              <InputLabel id="demo-simple-select-helper-label">Login</InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={user}
+                label="Age"
+                onChange={handleChange}
+                fullWidth={true}
+              >
+                <MenuItem value={'student'} >Student</MenuItem>
+                <MenuItem value={'teacher'} >Teacher</MenuItem>
+                <MenuItem value={'recruiter'} >Recruiter</MenuItem>
+              </Select>
+            </FormControl>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                autoComplete="current-password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleClickShowPassword}>
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: '#F39223',  width: '100%', marginTop: '10px', '&:hover': { backgroundColor: '#ef860d'  }}}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
 
 export default Login
